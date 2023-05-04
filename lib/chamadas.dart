08912ab15eb4b78558df629e7d0f2ea994cdcbe2
cesarifms <127432432+cesarifms@users.noreply.github.com>
2023-05-04 18:08:41 -0400

    Add files via upload

---
+++ lib/chamadas.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Chamadas extends StatelessWidget {
+  const Chamadas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("Pagina de Status"),
+    );
+  }
+}
+
+
+
